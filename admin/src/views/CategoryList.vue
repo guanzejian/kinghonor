5f618f9e53b7b4551daa8e7bceb54b1d12c92faf
guan <guanzejian@shanghai.com.cn>
2019-07-29 10:20:16 +0800

    5

---
+++ admin/src/views/CategoryList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>分类列表</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ kinghonor/README.md
@@ -0,0 +1,7 @@
+# kinghonor
+> 在 文件中全局安装vue和cli
+   npm install -g @vue/cli
+> 安装后，创建webclient文件（带cli的模板）
+   vue create webclient 
+> server 端，安装nodemon
+   npm i -g nodemon
